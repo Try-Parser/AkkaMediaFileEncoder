c26ad8efd760cdcb8d564f90dadc2b2202983619
F <franklin@hubbub.ai>
2020-08-25 18:56:13 +0800

    update

--- media-manager-service/src/main/scala/routes/ServerRoutes.scala
+++ media-manager-service/src/main/scala/routes/ServerRoutes.scala
@@ -3,7 +3,6 @@ package media.service.routes
 import java.io.File
 import java.time.Instant
 import java.util.UUID
-// import java.nio.file.Paths
 
 import scala.concurrent.duration._
 import scala.util.{ Success, Failure }
@@ -12,7 +11,6 @@ import akka.actor.typed.ActorSystem
 
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-// import akka.http.scaladsl.model.StatusCodes
 
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 
@@ -22,9 +20,6 @@ import media.service.handler.FileActorHandler
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.directives.FileInfo
-
-// import akka.stream.scaladsl.FileIO
-// import akka.stream.Materializer
 
 import com.typesafe.config.ConfigFactory
 
@@ -41,13 +36,11 @@ private[service] final class ServiceRoutes(system: ActorSystem[_]) extends Spray
 		.toMillis
 		.millis
 
-	// implicit private val mat: Materializer = Materializer(system.classicSystem)
 
 	//handler
 	val fileActorHandler: media.service.handler.FileActorHandler = FileActorHandler(sharding)
 
 	val configPath: String = ConfigFactory.load().getString("upload.path")
-		// "/home/frank/Desktop/file"
 
 	def tmpDst(file: FileInfo)(implicit tmpName: String): File = 
 		File.createTempFile(tmpName, ".tmp", new File(configPath))
