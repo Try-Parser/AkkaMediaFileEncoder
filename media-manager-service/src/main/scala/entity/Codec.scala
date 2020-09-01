a49ede0dd7d30e676d85b22d24f317af2ebeee1d
F <franklin@hubbub.ai>
2020-09-01 17:41:45 +0800

    update

--- media-manager-service/src/main/scala/entity/Codec.scala
+++ media-manager-service/src/main/scala/entity/Codec.scala
@@ -1,6 +1,6 @@
 package media.service.entity
 
-import ws.schild.jave.encode.{AudioAttributes, VideoAttributes}
+import ws.schild.jave.encode.{ AudioAttributes, VideoAttributes }
 import ws.schild.jave.info.VideoSize
 
 object Codec {
@@ -17,39 +17,38 @@ object Codec {
 	final case class FrameRate(value: Int) extends AnyVal
 	final case class PixelFormat(value: String) extends AnyVal
 
-	final case class AudioAttr(codec: CodecName = CodecName(""),
-			                       bitRate: BitRate = BitRate(0),
-	                           samplingRate: SamplingRate = SamplingRate(0),
-	                           channels: Channels = Channels(0),
-	                           volume: Volume = Volume(0),
-	                           quality: Quality = Quality(0)) {
-
-		def toJaveAudioAttr: AudioAttributes = {
-			val audio: AudioAttributes = new AudioAttributes()
-			NonEmptyString(codec.value).map{a => audio.setCodec(a)}
-			NonZeroInt(bitRate.value).map(a => audio.setBitRate(a))
-			NonZeroInt(samplingRate.value).map(a => audio.setSamplingRate(a))
-			NonZeroInt(channels.value).map(a => audio.setChannels(a))
-			NonZeroInt(volume.value).map(a => audio.setVolume(a))
-			NonZeroInt(quality.value).map(a => audio.setQuality(a))
-			audio
-		}
+	final case class AudioAttr(
+		codec: CodecName = CodecName(""),
+		bitRate: BitRate = BitRate(0),
+		samplingRate: SamplingRate = SamplingRate(0),
+		channels: Channels = Channels(0),
+		volume: Volume = Volume(0),
+		quality: Quality = Quality(0)) {
+			def toJaveAudioAttr: AudioAttributes = {
+				val audio: AudioAttributes = new AudioAttributes()
+				NonEmptyString(codec.value).map{a => audio.setCodec(a)}
+				NonZeroInt(bitRate.value).map(a => audio.setBitRate(a))
+				NonZeroInt(samplingRate.value).map(a => audio.setSamplingRate(a))
+				NonZeroInt(channels.value).map(a => audio.setChannels(a))
+				NonZeroInt(volume.value).map(a => audio.setVolume(a))
+				NonZeroInt(quality.value).map(a => audio.setQuality(a))
+				audio
+			}
 	}
 
-
-	final case class VideoAttr(size: VideoSize,
-	                           codec: CodecName = CodecName(""),
-	                           bitRate: BitRate = BitRate(0),
-	                           frameRate: FrameRate = FrameRate(0)) {
-
-		def toJaveVideoAttr: VideoAttributes = {
-			val video: VideoAttributes = new VideoAttributes()
-			NonEmptyString(codec.value).map(v => video.setCodec(v))
-			NonZeroInt(bitRate.value).map(v => video.setBitRate(v))
-			NonZeroInt(frameRate.value).map(v => video.setFrameRate(v))
-			video.setSize(size)
-			video
-		}
+	final case class VideoAttr(
+		size: VideoSize,
+		codec: CodecName = CodecName(""),
+		bitRate: BitRate = BitRate(0),
+		frameRate: FrameRate = FrameRate(0)) {
+			def toJaveVideoAttr: VideoAttributes = {
+				val video: VideoAttributes = new VideoAttributes()
+				NonEmptyString(codec.value).map(v => video.setCodec(v))
+				NonZeroInt(bitRate.value).map(v => video.setBitRate(v))
+				NonZeroInt(frameRate.value).map(v => video.setFrameRate(v))
+				video.setSize(size)
+				video
+			}
 	}
 
 }
---
+++ media-manager-service/src/main/scala/handlers/FileHandler.scala
@@ -0,0 +1,50 @@
+package media.service.handlers 
+
+import java.nio.file.Paths
+import java.io.File
+
+import java.nio.file.Paths
+
+import scala.concurrent.Future
+
+import akka.util.ByteString
+import akka.http.scaladsl.model.HttpEntity.{ Chunked, ChunkStreamPart }
+import akka.http.scaladsl.model.{ ContentTypes, ResponseEntity }
+import akka.stream.IOResult
+import akka.stream.scaladsl.{ FileIO, Source }
+import akka.stream.Materializer
+
+import ws.schild.jave.MultimediaObject
+
+private[service] object FileHandler {
+	private val factory: com.typesafe.config.Config = 
+		com.typesafe.config.ConfigFactory.load()
+
+	val basePath: String = factory.getString("upload.path")
+	val maxContentSize: Long = factory.getLong("upload.max-content-size")
+
+	def getFile(name: String): File = new File(s"${basePath}/$name")
+
+	def getChunked(name: String): ResponseEntity  = 
+		Chunked(ContentTypes.`application/octet-stream`, getSource(name)) 
+
+	def getMultiMedia(name: String): MultimediaObject = getMultiMedia(getFile(name))
+
+	def getMultiMedia(file: File): MultimediaObject = new MultimediaObject(file)
+
+	def getXtn(fileName: String): String = {
+		val fullName: Array[String] = fileName.split("\\.")
+		if(fullName.size <= 1) "tmp" else fullName(fullName.size-1)
+	}
+
+	def getFileWithPath(fileName: String): Source[ByteString, Future[IOResult]] =
+		FileIO.fromPath(Paths.get(s"${basePath}/$fileName"))
+
+	def getSource(name: String): Source[ChunkStreamPart, Future[IOResult]] =
+		getFileWithPath(name).map(ChunkStreamPart.apply)
+
+	def writeFile(
+		fileName: String, 
+		source: Source[ByteString, _])(implicit mat: Materializer): Future[IOResult] =
+			source.runWith(FileIO.toPath(Paths.get(s"${FileHandler.basePath}/$fileName")))
+}
